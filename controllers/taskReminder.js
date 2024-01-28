a27c0253ca71bb455373caffdb5ec8fef7323e24
prakash-s-2210 <sbprakash.kvs@gmail.com>
2024-01-28 17:49:18 +0530

    added twilio webhook

--- controllers/taskReminder.js
+++ controllers/taskReminder.js
@@ -39,19 +39,26 @@ export const taskReminder = async (req, res) => {
     const userToCall = await getSingleUserToCall();
 
     // Step 4: Make call to user
-      const accountSid = process.env.ACCOUNT_SID;
-      const authToken = process.env.AUTH_TOKEN;
-      const client = twilio(accountSid, authToken);
+    const accountSid = process.env.ACCOUNT_SID;
+    const authToken = process.env.AUTH_TOKEN;
+    const client = twilio(accountSid, authToken);
 
-      const call = await client.calls.create({
-        statusCallback: "https://task-management-145y.onrender.com/events",
-        statusCallbackEvent: ["initiated", "answered"],
-        statusCallbackMethod: "POST",
-        url: "http://demo.twilio.com/docs/voice.xml",
-        to: `+91${userToCall.phoneNumber}`,
-        from: "+13153524686",
-      });
+    const call = await client.calls.create({
+      statusCallback: "https://task-management-145y.onrender.com/events",
+      statusCallbackEvent: ["initiated", "answered"],
+      statusCallbackMethod: "POST",
+      url: "http://demo.twilio.com/docs/voice.xml",
+      to: `+91${userToCall.phoneNumber}`,
+      from: "+13153524686",
+    });
+
+    res
+      .status(200)
+      .json({ message: "Successfully updated task priorities and started making call to the user", task: udpatedTask });
   } catch (error) {
-    console.error("Error updating task priorities and making calls ", error.message);
+    console.error(
+      "Error updating task priorities and making calls ",
+      error.message
+    );
   }
 };
